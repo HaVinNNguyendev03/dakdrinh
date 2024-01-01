f186e94668cb7c52f246ec0aa1099ace02ece891
MAY88\ADMIN <nguyenvanphucdev03@gmail.com>
2024-01-01 14:40:45 +0700

    commit phuc 3

--- app/Views/home.php
+++ app/Views/home.php
@@ -60,7 +60,6 @@
                                             stroke-width="2" d="m1 1 4 4 4-4" />
                                     </svg>
                                 </a>
-
                                 <!-- Dropdown menu -->
                                 <div id="dropdown1"
                                     class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
@@ -162,7 +161,7 @@
                             <li>
                                 <a id="dropdownDefaultButton3" data-dropdown-toggle="dropdown3"
                                     class="text-white hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm  text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                    type="button">QUAN HỆ CỔ ĐÔNG<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                    type="button">QUAN HỆ CỔ ĐÔNG testcommit<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                             stroke-width="2" d="m1 1 4 4 4-4" />
@@ -176,7 +175,7 @@
                                         aria-labelledby="dropdownDefaultButton3">
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard1234343</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                                         </li>
                                         <li>
                                             <a href="#"
@@ -184,7 +183,7 @@
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings121213</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
                                         </li>
                                         <li>
                                             <a href="#"
