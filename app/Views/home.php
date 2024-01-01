913cc657727232913b34e8b92cc96763b76a7788
MAY88\ADMIN <nguyenvanphucdev03@gmail.com>
2024-01-01 14:58:11 +0700

    commit fix home

--- app/Views/home.php
+++ app/Views/home.php
@@ -79,7 +79,7 @@
                                         </li>
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">tin tức PV Powr DHC</a>                                            out</a>
+                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tin tức PV Power DHC</a>
                                         </li>
                                     </ul>
                                 </div>
