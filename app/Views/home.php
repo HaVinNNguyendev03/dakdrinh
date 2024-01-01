9247d1e76829e0298913e891208241e938499a47
PHUOC-PC\Phuoc PC <phuocdhc2023@gmail.com>
2024-01-01 14:47:40 +0700

    commit phước 4

--- app/Views/home.php
+++ app/Views/home.php
@@ -67,7 +67,7 @@
                                         aria-labelledby="dropdownDefaultButton1">
                                         <li>
                                             <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hoạt động cộng đồng/a>
+                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hoạt động cộng đồng</a>
                                         </li>
                                         <li>
                                             <a href="#"
@@ -160,7 +160,7 @@
                             <li>
                                 <a id="dropdownDefaultButton3" data-dropdown-toggle="dropdown3"
                                     class="text-white hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm  text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                    type="button">QUAN HỆ CỔ ĐÔNG testcommit<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                    type="button">QUAN HỆ CỔ ĐÔNG<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                             stroke-width="2" d="m1 1 4 4 4-4" />
